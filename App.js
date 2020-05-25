491b8fd7f6d1d1f3a770d8441841b779fe09be4e
suyashshukla <suyashshukla1997@gmail.com>
2020-05-25 11:18:50 +0530

    Migration

--- App.js
+++ App.js
@@ -8,54 +8,103 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, Image, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
+import {Platform, StyleSheet, Text,
+    Image,ScrollView, View,Switch,TouchableOpacity,Button} from 'react-native';
 
 
-export default class Second extends Component{
+let id = 0;
 
-  render() {
-      let pic = {
-          uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-      };
+export default class Second extends Component {
 
-          return (
 
-        <View style={styles.container}>
-          <Text style={styles.welcome}>Welcome to Qin App!</Text>
-          <Text style={styles.instructions}>Attendance made easy!</Text>
-          <Text style={styles.instructions}>{instructions}</Text>
-            <Image source={pic} style={{width: 193, height: 110}}/>
-        </View>
-    )
-  };
+    constructor() {
+        super();
+
+        this.state = {
+            todo: [],
+        };
+    }
+
+    render() {
+
+        return (
+
+            <View style={{
+                flexDirection: 'column',
+                margin: 50
+            }}>
+                {/*<Text style={styles.proper}*/}
+                {/*onPress={this.change}>{this.state.data}</Text>*/}
+
+                {/*<Text style={styles.proper}>Attendance made easy!</Text>*/}
+                {/*<Text style={styles.proper}>{instructions}</Text>*/}
+
+                <TouchableOpacity
+                    style={{backgroundColor : '#DDF567'}}
+                    onPress={() => this.addData()}>
+                    <Text
+                        style={{alignContent:'centre'}}
+                    >ADD</Text>
+                </TouchableOpacity>
+
+                <ScrollView>
+                    {this.state.todo.map(todo =>(
+                        <Todo
+                            onChange={() => this.checkChange(todo.id)}
+                            onRemove={() => this.removeData(todo.id)}
+                            todo = {todo}/>
+                    ))}
+                </ScrollView>
+            </View>
+        )
+    }
+
+
+    checkChange(id) {
+
+        this.setState({todo : this.state.todo.map(
+                todo =>
+                {
+
+                    if(todo.id!==id)
+                        return todo;
+
+                    return{
+                        text: "Open",
+                        id: todo.id,
+                        checked: !todo.checked,
+                    };
+
+                }
+            )}
+        )}
+
+
+    addData(){
+        id += 1;
+        this.setState({todo: [...this.state.todo,{id : id,checked : false,text:'New Item!'}]
+        })
+    }
+
+    removeData(id){
+        this.setState({
+            todo : this.state.todo.filter(todo =>
+            {
+                if(todo.id!==id)
+                    return todo;
+            })
+        })
+    }
 }
 
+const Todo = (props) => (
 
+    <View style={{flexDirection: 'column', flex:1}}>
+        <Switch
+            value={props.todo.checked}
+            onValueChange={() => props.onChange()}/>
+        <Text>{props.todo.text}</Text>
+        <Button title={"Delete"} onPress={() => props.onRemove()}/>
+    </View>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
